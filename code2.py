15a3273a8d5ec5df7d4b6a2b893a0b0ce823dcba
zhuhl98 <zhuhl98@gmail.com>
2022-04-02 12:45:02 -0700

    Update code2.py

--- code2.py
+++ code2.py
@@ -1,3 +1,4 @@
+from tkinter import constants
 import numpy as np
 import math
 import matplotlib.pyplot as plt
@@ -13,11 +14,11 @@ parser = argparse.ArgumentParser(description='Setting learning rate type')
 parser.add_argument("-l", "--L",\
       help="value of lambda", type = float, default=0.5)
 parser.add_argument("-t", "--T",\
-     help="Time steps", type = int, default=1000000)
+     help="Time steps", type = int, default=100)
 args = parser.parse_args()
 # print(args.learning_rate_type)
 print(args.T)
-print(args.l)
+print(args.L)
 # print(args.C)
 
 K, d = 5, 10
@@ -48,6 +49,7 @@ def f(x):
     return max([f_k(x, k) for k in range(K)])
 
 T = args.T
+lamb = args.L
 # C = args.C
 
 x1 = np.ones((10), dtype = float)
@@ -68,20 +70,41 @@ def norm(x):
 
 f_iter = [f(x1)]
 f_best = [f(x1)]
+f_plus = f(x1)
 
 # a guess for optimal f which is computed in HW 1
 f_star_guess = -0.8414004108659314
 
-for t in range(T-1):
-    if t % 10000 == 0:
-        print('Training iter: {}'.format(t))
+def solve_f_minus():
+    x = cp.Variable(d)
+    
+    # print(max([f(xj) + cp.sum(cp.multiply(sub_gradient(xj),x-xj)) for xj in x_iter]))
+    print(cp.max(cp.hstack(f(xj) + cp.sum(cp.multiply(sub_gradient(xj),x-xj)) for xj in x_iter)))
+    obj = cp.Minimize(cp.max(cp.hstack(f(xj) + cp.sum(cp.multiply(sub_gradient(xj),x-xj)) for xj in x_iter)))
+    prob = cp.Problem(obj)
+    prob.solve()
+    return prob.value
+
+def proj(l):
+    x_old = x_iter[-1]
+    x_new = cp.Variable(d)
+    obj = cp.Minimize(cp.sum(cp.square(x_old - x_new)))
+    constraints = [cp.max(cp.hstack(f(xj) + cp.sum(cp.multiply(sub_gradient(xj),x-xj)) for xj in x_iter)) <= l]
+    try:
+        prob = cp.Problem(obj, constraints)
+        prob.solve()
+    except:
+        return x_old
+    return x_new.value
+
+for t in trange(T-1, desc="level method", unit="sec"):
     x = x_iter[-1]
-    g = sub_gradient(x) 
-    if args.learning_rate_type == 'c/sqrt(t)':
-        eta = C/math.sqrt(t+1)
-    else:
-        eta = (f(x) - f_star_guess) / norm(g)
-    x = x - eta * g / norm(g)
+
+    f_minus = solve_f_minus()
+    f_plus = min(f_plus, f(x))
+    l = (1-lamb)*f_minus + lamb*f_plus
+    x = proj(l)
+
     x_iter.append(x)
     f_iter.append(f(x))
     f_best.append(min(f_best[-1],f(x)))
@@ -94,5 +117,5 @@ t_iter = np.arange(1,T+1, 1)
 plt.loglog(t_iter, f_best - np.min([f_opt, f_star_guess]))
 plt.xlabel('# of iteration')
 plt.ylabel('sub-optimality gap')
-plt.title('Convergence rate of level method for lambda = {}'.format(args.l))
+plt.title('Convergence rate of level method for lambda = {}'.format(lamb))
 plt.show()
